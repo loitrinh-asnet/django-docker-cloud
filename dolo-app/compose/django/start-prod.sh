839d4ef013f296181202caeb99e5540bf3b9672e
Loi Trinh <loi.trinhxuan@asnet.com.vn>
2017-11-01 17:45:47 +0700

    Continue to build the docker compose stacks
     - Backend app (Django)
    - Database (Postgres)
    - Nginx
    - Redis

---
+++ dolo-app/compose/django/start-prod.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#
+python /app/manage.py collectstatic --noinput
+/usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app
--- dolo-app/config/settings/prod.py
+++ dolo-app/config/settings/prod.py
@@ -1,3 +1,9 @@
 """Prod settings for dolo-app project."""
 
 from .base import *  # noqa
+
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['localhost', ])
+
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='y7Fxywd1JpgR9dY&^<Nl5SDL@u2L4@11(,8hQaH;>wtUIoClzU')
+
+INSTALLED_APPS += ['gunicorn', ]
