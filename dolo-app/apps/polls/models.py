157bfab5cc9c12de9b01c16d83eb27299673a6a2
Loi Trinh <loi.trinhxuan@asnet.com.vn>
2017-11-02 15:17:17 +0700

    Corrected the bug in the code to all test passes, the next time, automatically push the built image to the Docker repository.

--- dolo-app/apps/polls/models.py
+++ dolo-app/apps/polls/models.py
@@ -1,4 +1,6 @@
 """Models."""
+import datetime
+from django.utils import timezone
 from django.db import models
 
 
@@ -7,6 +9,11 @@ class Question(models.Model):
 
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        """was_published_recently."""
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
